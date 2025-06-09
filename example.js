57be7a58e8ec230257da945514c4a3bcc380f6ae
SridharMR24 <sridhar24eee@gmail.com>
2025-06-09 11:15:27 +0530

    truthy and falsy value

--- example.js
+++ example.js
@@ -1,5 +1,15 @@
-console.log("2">"3");
-console.log("mango">"banana");
-console.log(null == undefined);
-console.log(undefined === null);
-console.log(null < 1);
+// console.log("2">"3");
+// console.log("mango">"banana");
+// console.log(null == undefined);
+// console.log(undefined === null);
+// console.log(null < 1);
+
+const firstname = "Sridhar"
+const nickname = ""
+const username = firstname && nickname && "hi";
+console.log(`Name - ${username}`);
+
+const firstName = ""
+const nickName = ""
+const userName = firstName || nickName || null;
+console.log(`Name - ${userName}`);
